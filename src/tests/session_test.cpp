4ed08135a4b9a5df905dbca48a19d551e2390370
Khaled Moussa <khaled.gnu@gmail.com>
2020-11-30 01:13:51 +0100

    Adding 2 test cases

--- src/tests/session_test.cpp
+++ src/tests/session_test.cpp
@@ -15,14 +15,47 @@ void SessionTest::TearDown() {}
 void SessionTest::TestBody() {}
 
 
+TEST_F(SessionTest, createsuperuser) {
+        SessionTest s;
+	Admin *super = new Admin();
+	super->setId(1);
+	super->setUserName("ahmed");
+	super->setFirstName("Ahmed");
+	super->setLastName("Mostafa");
+	super->setNationalId("2222222");
+	super->setUserType(1);
+	string password = s.sess->encrypt("abc123");
+	super->setPassword(password);
+	super->cap_AdminCreate(true);
+	super->cap_AdminUpdate(true);
+	super->cap_AdminDelete(true);
+	super->cap_AdminActivate(true);
+	super->cap_AdminDeactivate(true);
+	super->cap_AdminPrintInfo(true);
+	super->cap_AdminListAll(true);
 
-TEST_F(SessionTest, AdminLogin) {
-	SessionTest s;
-	EXPECT_TRUE(s.sess->login("kmoussa", "intruder"));
+	EXPECT_TRUE(s.sess->createAdmin(super));
+
 }
 
+TEST_F(SessionTest, createadminuserNotLogged) {
+    	SessionTest s;
+	Admin *admin = new Admin();
+	admin->setId(2);
+	admin->setUserName("shimaa");
+	admin->setFirstName("Shimaa");
+	admin->setLastName("Mostafa");
+	admin->setNationalId("12121212");
+	admin->setUserType(1);
+	string password = s.sess->encrypt("abc123");
+	admin->setPassword(password);
+	admin->cap_AdminCreate(true);
+	admin->cap_AdminUpdate(true);
+	admin->cap_AdminDelete(true);
+	admin->cap_AdminActivate(true);
+	admin->cap_AdminDeactivate(true);
+	admin->cap_AdminPrintInfo(true);
+	admin->cap_AdminListAll(true);
 
-TEST_F(SessionTest, EmpLogin) {
-        SessionTest s;
-        EXPECT_TRUE(s.sess->login("amira", "abc123"));
+	EXPECT_FALSE(s.sess->createAdmin(admin));
 }
